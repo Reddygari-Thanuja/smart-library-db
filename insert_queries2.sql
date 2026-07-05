eeaac84527709e1a9718b9ad42dd0b4b53423260
Reddygari-Thanuja <rthanujareddy@gmail.com>
2026-07-05 10:54:53 +0530

    Insert records and modify return_status table
    Inserted new records into issued_status and added a book_quality column to return_status with an update for specific issued_ids.

--- insert_queries2.sql
+++ insert_queries2.sql
@@ -1,8 +1,3 @@
--- INSERT INTO book_issued in last 30 days
--- SELECT * from employees;
--- SELECT * from books;
--- SELECT * from members;
--- SELECT * from issued_status
 
 
 INSERT INTO issued_status(issued_id, issued_member_id, issued_book_name, issued_date, issued_book_isbn, issued_emp_id)
@@ -12,7 +7,6 @@ VALUES
 ('IS153', 'C106', 'Pride and Prejudice', CURRENT_DATE - INTERVAL '7 days',  '978-0-14-143951-8', 'E107'),
 ('IS154', 'C105', 'The Road', CURRENT_DATE - INTERVAL '32 days',  '978-0-375-50167-0', 'E101');
 
--- Adding new column in return_status
 
 ALTER TABLE return_status
 ADD Column book_quality VARCHAR(15) DEFAULT('Good');
@@ -22,4 +16,3 @@ SET book_quality = 'Damaged'
 WHERE issued_id 
     IN ('IS112', 'IS117', 'IS118');
 SELECT * FROM return_status;
-
